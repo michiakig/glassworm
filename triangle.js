fab04665ee6dc8be2095e66883a48e4d82e25472
aki <spacemanaki@gmail.com>
2013-07-16 19:20:50 -0400

    added interactivity

--- triangle.js
+++ triangle.js
@@ -1,8 +1,36 @@
 ;(function(window) {
     var Utils = window.Utils;
+
+    var requestAnimationFrame =
+            window.requestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+
+    var canvas;
+    var gl;
+    var x=0, y=0;
+
+    function handle(evt) {
+        switch(evt.keyCode) {
+            case 37: x--; break;
+            case 38: y--; break;
+            case 39: x++; break;
+            case 40: y++; break;
+        }
+    }
+
+    function main() {
+        canvas = document.getElementById('canvas');
+        gl = canvas.getContext('experimental-webgl');
+
+        document.body.addEventListener('keydown', handle);
+        draw();
+    }
+
     function draw() {
-        var canvas = document.getElementById('canvas');
-        var gl = canvas.getContext('experimental-webgl');
+        requestAnimationFrame(draw);
 
         // set up shaders
         var vs = document.getElementById('vertex').textContent;
@@ -10,12 +38,11 @@
         var program = Utils.createProgram(gl, vs, fs);
         gl.useProgram(program);
 
-       // var p = proj(canvas.width, canvas.height, canvas.width);
-       // var loc = gl.getUniformLocation(program, 'uproj');
-       // gl.uniformMatrix4fv(loc, false, new Float32Array(p.data));
+        var p = proj(canvas.width, canvas.height, canvas.width);
+        var loc = gl.getUniformLocation(program, 'uproj');
+        gl.uniformMatrix4fv(loc, false, new Float32Array(p.data));
 
-//        Utils.pushData(gl, [100, 300, 300, 300, 200, 100]);
-        Utils.pushData(gl, [-0.5, -0.5, 0.5, -0.5, 0, 0.5]);
+        Utils.pushData(gl, [100+x, 300+y, 300+x, 300+y, 200+x, 100+y]);
         Utils.updateAttrib(gl, program, 'pos', 2);
 
         Utils.pushData(gl, [
@@ -29,6 +56,7 @@
     }
 
     window.Triangle = {
+        main: main,
         draw: draw
     };
 })(window);
