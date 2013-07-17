330fe33bbe864e63c08d59c58842c28d22ff1798
aki <spacemanaki@gmail.com>
2013-07-17 17:07:42 -0400

    added some 3d

--- triangle.js
+++ triangle.js
@@ -10,7 +10,7 @@
 
     var canvas;
     var gl;
-    var x=0, y=0, r=0;
+    var x=0, y=0, rx=0, ry=0, rz=0;
 
     function handle(evt) {
         switch(evt.keyCode) {
@@ -19,8 +19,14 @@
             case 39: x+=10; break;
             case 40: y+=10; break;
 
-            case 74: r+=10; break;
-            case 75: r-=10; break;
+            case 74: rx+=10; break;
+            case 75: rx-=10; break;
+
+            case 78: ry+=10; break;
+            case 77: ry-=10; break;
+
+            case 85: rz+=10; break;
+            case 73: rz-=10; break;
 
             default: console.log('handle:'+evt.keyCode); break;
         }
@@ -45,21 +51,51 @@
 
         var p = proj(canvas.width, canvas.height, canvas.width);
         p = p.mul(trans(x, y, 0));
-        p = p.mul(rotZ(r * Math.PI / 180));
+        p = p.mul(rotX(rx * Math.PI / 180));
+        p = p.mul(rotY(ry * Math.PI / 180));
+        p = p.mul(rotZ(rz * Math.PI / 180));
         var loc = gl.getUniformLocation(program, 'uproj');
         gl.uniformMatrix4fv(loc, false, new Float32Array(p.data));
 
-        Utils.pushData(gl, [0, 0, -50, 100, 50, 100]);
-        Utils.updateAttrib(gl, program, 'pos', 2);
+        Utils.pushData(gl, [
+            0,  0,   0,
+           -50, 86.6, 50,
+            50, 86.6, 50,
+
+            0,  0,   0,
+            50, 86.6, 50,
+            0,  86.6,-50,
+
+            0,  0,   0,
+            0,  86.6,-50,
+            -50,86.6, 50,
+
+            0,  86.6,-50,
+            -50,86.6, 50,
+            50, 86.6, 50
+        ]);
+        Utils.updateAttrib(gl, program, 'pos', 3);
 
         Utils.pushData(gl, [
+            0.8, 0.8, 0, 1,
+            0.8, 0.8, 0, 1,
+            0.8, 0.8, 0, 1,
+
+            0, 0.8, 0.8, 1,
+            0, 0.8, 0.8, 1,
+            0, 0.8, 0.8, 1,
+
+            0.8, 0, 0.8, 1,
+            0.8, 0, 0.8, 1,
+            0.8, 0, 0.8, 1,
+
             1, 0, 0, 1,
-            0, 1, 0, 1,
-            0, 0, 1, 1
+            1, 0, 0, 1,
+            1, 0, 0, 1
         ]);
         Utils.updateAttrib(gl, program, 'acolor', 4);
 
-        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        gl.drawArrays(gl.TRIANGLES, 0, 12);
     }
 
     window.Triangle = {
