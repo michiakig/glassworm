d2d345d9db32d6d0ea40bb633ca9972f1ac27958
aki <spacemanaki@gmail.com>
2013-07-28 21:25:07 -0400

    inane example of calling gl.drawArrays twice with different indices

--- scratch.js
+++ scratch.js
@@ -1,65 +1,39 @@
 ;(function(window) {
-    function drawRectangle(gl, program, color, x, y, w, h) {
-        var geo = [  x,     y,     0,
-                     x + w, y,     0,
-                     x,     y + h, 0,
-                     x + w, y + h, 0 ];
-        Utils.pushData(gl, geo);
-        Utils.updateAttrib(gl, program, 'pos', 3);
-
-        Utils.pushData(gl, color);
-        Utils.updateAttrib(gl, program, 'acolor', 4);
-
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-    }
-
-    function drawTriangle(gl, program, color, x, y) {
-        var geo = [  0 + x,  0.5 + y, 0,
-                  -0.5 + x, -0.5 + y, 0,
-                   0.5 + x, -0.5 + y, 0 ];
-        Utils.pushData(gl, geo);
-        Utils.updateAttrib(gl, program, 'pos', 3);
-
-        Utils.pushData(gl, color);
-        Utils.updateAttrib(gl, program, 'acolor', 4);
-
-        gl.drawArrays(gl.TRIANGLES, 0, 3);
-    }
-
     function main() {
+        // get gl context
         var canvas = document.getElementById('canvas');
         var gl = canvas.getContext('experimental-webgl', {preserveDrawingBuffer: true});
-        window.canvas = canvas;
-        window.gl = gl;
 
+        // prepare shader code
         var vs = document.getElementById('vertex').textContent;
         var fs = document.getElementById('fragment').textContent;
         var program = Utils.createProgram(gl, vs, fs);
-        window.program = program;
         gl.useProgram(program);
 
         var p = proj(canvas.width, canvas.height, canvas.width);
         var loc = gl.getUniformLocation(program, 'uproj');
         gl.uniformMatrix4fv(loc, false, new Float32Array(p.data));
 
-        drawRectangle(gl, program, [0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1], 100, 100, 3, 50);
-        drawRectangle(gl, program, [0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1], 100, 100, 50, 3);
-        drawRectangle(gl, program, [0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1], 147, 100, 3, 50);
-        drawRectangle(gl, program, [0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1,
-                                    0, 0.8, 0, 1], 100, 150, 50, 3);
+        var triangles = [
+            50, 0, 0,
+            0, 100, 0,
+            100, 100, 0,
 
+            300, 400, 0,
+            350, 300, 0,
+            400, 400, 0
+        ];
+
+        var buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangles), gl.STATIC_DRAW);
+
+        loc = gl.getAttribLocation(program, 'pos');
+        gl.enableVertexAttribArray(loc);
+        gl.vertexAttribPointer(loc, 3, gl.FLOAT, false, 0, 0);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        gl.drawArrays(gl.TRIANGLES, 3, 3);
     }
     window.main = main;
-    window.drawTriangle = drawTriangle;
 })(window);
