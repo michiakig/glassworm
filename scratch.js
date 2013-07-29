caf69afb6e93bc83e1816e16324f88e47757439f
aki <spacemanaki@gmail.com>
2013-07-28 21:53:47 -0400

    again, screwing around with the order of things and multiple calls to gl.drawArrays

--- scratch.js
+++ scratch.js
@@ -14,19 +14,21 @@
         var loc = gl.getUniformLocation(program, 'uproj');
         gl.uniformMatrix4fv(loc, false, new Float32Array(p.data));
 
-        var triangles = [
+        var triangles1 = [
             50, 0, 0,
             0, 100, 0,
-            100, 100, 0,
+            100, 100, 0
+        ];
 
-            300, 400, 0,
-            350, 300, 0,
-            400, 400, 0
+        var triangles2 = [
+            150, 0, 0,
+            100, 100, 0,
+            200, 100, 0
         ];
 
         var buffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangles), gl.STATIC_DRAW);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangles1), gl.STATIC_DRAW);
 
         loc = gl.getAttribLocation(program, 'pos');
         gl.enableVertexAttribArray(loc);
@@ -34,11 +36,19 @@
 
         gl.drawArrays(gl.TRIANGLES, 0, 3);
 
-        p = proj(canvas.width*2, canvas.height*2, canvas.width*2);
-        loc = gl.getUniformLocation(program, 'uproj');
-        gl.uniformMatrix4fv(loc, false, new Float32Array(p.data));
+        var buffer2 = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer2);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangles2), gl.STATIC_DRAW);
 
-        gl.drawArrays(gl.TRIANGLES, 3, 3);
+        loc = gl.getAttribLocation(program, 'pos');
+        gl.enableVertexAttribArray(loc);
+        gl.vertexAttribPointer(loc, 3, gl.FLOAT, false, 0, 0);
+
+        // p = proj(canvas.width*2, canvas.height*2, canvas.width*2);
+        // loc = gl.getUniformLocation(program, 'uproj');
+        // gl.uniformMatrix4fv(loc, false, new Float32Array(p.data));
+
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
     }
     window.main = main;
 })(window);
